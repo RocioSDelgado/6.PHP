a2be758bea9ac1b4db3607cc4b1408f781412af2
Rocio Delgado Crespo <122272150+RocioSDelgado@users.noreply.github.com>
2023-05-14 12:55:57 +0200

    Add files via upload

---
+++ DB/CursoSQL.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 14-05-2023 a las 12:54:52
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `CursoSQL`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `USUARIA`
+--
+
+CREATE TABLE `USUARIA` (
+  `ID` int(11) NOT NULL,
+  `NOMBRE` varchar(200) NOT NULL,
+  `APELLIDO` varchar(200) NOT NULL,
+  `EMAIL` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `USUARIA`
+--
+
+INSERT INTO `USUARIA` (`ID`, `NOMBRE`, `APELLIDO`, `EMAIL`) VALUES
+(1, 'Rocio', 'Delgado', 'rociodel@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `USUARIA`
+--
+ALTER TABLE `USUARIA`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `USUARIA`
+--
+ALTER TABLE `USUARIA`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
